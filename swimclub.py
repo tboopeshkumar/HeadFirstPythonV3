56dbe79803589814f650027c9c7f76ac8dc58908
Boopesh Kumar <tboopeshkumar@gmail.com>
2026-01-07 22:30:39 +0400

    learning chapter 4

--- swimclub.py
+++ swimclub.py
@@ -3,13 +3,23 @@ import statistics
 FOLDER = "swimdata/"
 
 def read_swim_data(filename):
+    """Return swim data from a file.
+
+    Given the name of. a swimmer's file (in filename), extract all the required
+    data, then return it to the caller as tuple.    
+    """
     swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
     with open(FOLDER+filename) as file:
         lines = file.readlines()
         times = lines[0].strip().split(",")
     converts = []
     for t in times:
-        minutes, rest = t.split(":")
+        # The minutes value might be missing, so guard against this causing a crash.
+        if ":" in t:
+            minutes, rest = t.split(":")
+        else:
+            minutes = 0
+            rest = t
         seconds, hundredths = rest.split(".")
         converts.append((int(minutes)*60*100) + (int(seconds)* 100) + int(hundredths))
     average = statistics.mean(converts)
@@ -18,5 +28,5 @@ def read_swim_data(filename):
     minutes = mins_secs // 60
     seconds = mins_secs - minutes*60
     average = str(minutes)+ ":" +str(seconds)+"."+hundredths
-    return swimmer, age, distance, stroke, times, average
+    return swimmer, age, distance, stroke, times, average # Returned as a tuple.
 
