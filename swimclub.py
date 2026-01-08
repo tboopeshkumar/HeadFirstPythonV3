bc39ef6abd6b0c4e434b3699ed2c557bbd7cf242
Boopesh Kumar <tboopeshkumar@gmail.com>
2026-01-08 21:48:30 +0400

    chapter 5 learning

--- swimclub.py
+++ swimclub.py
@@ -23,10 +23,61 @@ def read_swim_data(filename):
         seconds, hundredths = rest.split(".")
         converts.append((int(minutes)*60*100) + (int(seconds)* 100) + int(hundredths))
     average = statistics.mean(converts)
-    mins_secs, hundredths = str(round(average / 100, 2)).split(".")
+    mins_secs, hundredths = f"{(average / 100):.2f}".split(".")
     mins_secs = int(mins_secs)
     minutes = mins_secs // 60
     seconds = mins_secs - minutes*60
-    average = str(minutes)+ ":" +str(seconds)+"."+hundredths
-    return swimmer, age, distance, stroke, times, average # Returned as a tuple.
+    average = f"{minutes}:{seconds:0>2}.{hundredths}"
+    return swimmer, age, distance, stroke, times, average, converts # Returned as a tuple.
+
+import hfpy_utils
+
+CHARTS = "charts/"
+
+def produce_bar_chart(fn):
+    """Given the name of the swimmer's file, produce a HTML/SVG-based bar chart.
+
+    Save the chart to the CHARTS folder. Return the path to the bar chart file.
+    """
+
+    data = read_swim_data(fn)
+    (swimmer, age, distance, stroke, times, average, converts) = data
+    title = f"{swimmer} (Under {age}) {distance} {stroke}"
+    from_max = max(converts)
+
+    html = f"""<!DOCTYPE html>
+    <html>
+        <head>
+            <title>{title}</title>
+        </head>
+        <body>
+        <h3>{title}</h3>
+    """
+
+    svgs = ""
+
+    times.reverse()
+    converts.reverse()
+
+    for n,t in enumerate(times):
+        bar_width = hfpy_utils.convert2range(converts[n], 0, from_max, 0, 350)
+        svgs = f"""{svgs}
+            <svg height="30" width="400">
+                <rect height="30" width="{bar_width}" style="fill:rgb(0,0,255);" />
+            </svg>{t}<br />
+        """
+    footer = f"""
+        <p>Average time: {average}</p>
+        </body>
+    </html>
+    """
+
+    page = f"{html}{svgs}{footer}"
+
+    save_to = f"{CHARTS}{fn.removesuffix('.txt')}.html"
+
+    with open(save_to, "w") as tf:
+        print(page, file=tf)
+    
+    return save_to
 
